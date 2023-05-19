0dc4dcbf3ef6baf647960bdb5569f58b60c2e7f4
matheusgomessouza <matheusg_souza@outlook.com>
2023-05-19 14:12:50 -0300

    feat: Finishing authentication logic

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,22 +1,11 @@
-import { styled } from 'nativewind';
 import { useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import * as SecureStore from 'expo-secure-store';
-import { ImageBackground, View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { useRouter } from "expo-router";
 
-import { 
-  useFonts, Roboto_400Regular, Roboto_700Bold 
-} from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { api } from '../src/lib/api';
-
-const StyledStripes = styled(Stripes)
 
 export default function App() {
   const router = useRouter();
@@ -43,15 +32,12 @@ export default function App() {
       code,
     })
     const { token } = response.data
-
-    SecureStore.getItemAsync('token', token)
-
+    SecureStore.setItemAsync('token', token)
     router.push('/memories')
   }
-
+  
   useEffect(() => {
-    // console.log(response)
-    // console.log(
+    // console.log('makeRedirectUri',
     //   makeRedirectUri({
     //     scheme: 'spacetime'
     //   })
@@ -59,30 +45,13 @@ export default function App() {
 
     if (response?.type === 'success') {
       const { code } = response.params;
-      
       handleGithubOAuthCode(code)
     }
   }, [response]);
 
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular, Roboto_700Bold, BaiJamjuree_700Bold
-  })
-
-
-
-  if (!hasLoadedFonts) {
-    return null
-  }
-
 
   return (
-    <ImageBackground 
-      source={blurBg} 
-      className='flex-1 relative items-center bg-gray-900 px-8 py-10'
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className='absolute left-2' />
-      
+    <View className='flex-1 items-center px-8 py-10'>
       <View className='flex-1 items-center justify-center gap-6'>
         <NLWLogo />
         <View className='space-y-2'>
@@ -108,8 +77,6 @@ export default function App() {
       <Text className='text-center font-body text-sm leading-relaxed text-gray-200'>
         Made with 💜 during NLW from Rocketseat
       </Text>
-
-      <StatusBar style="light" translucent/>
-    </ImageBackground>
+    </View>
   );
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,21 @@
 import 'dotenv/config'
 import fastify from 'fastify';
 import cors from '@fastify/cors';
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories';
 import { authRoutes } from './routes/auth';
 import jwt from '@fastify/jwt';
+import { resolve } from 'node:path';
+import { uploadRoutes } from './routes/upload';
 
 const app = fastify();
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true
@@ -13,6 +23,8 @@ app.register(cors, {
 app.register(jwt, {
   secret: 'spacetime'
 })
+
+app.register(uploadRoutes); 
 app.register(authRoutes); 
 app.register(memoriesRoutes); 
 
